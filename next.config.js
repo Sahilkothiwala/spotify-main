7f19992a596748bbac2738f5269344cd1b627cdb
Sahilkothiwla01 <89785191+Sahilkothiwla01@users.noreply.github.com>
2023-08-17 08:36:01 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: ['jyamxstpwxcyiailphgh.supabase.co', 'avatars.githubusercontent.com']
+    }
+}
+
+module.exports = nextConfig
